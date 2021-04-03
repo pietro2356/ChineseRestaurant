f5fdf3ffa668723026dfeda56437811ee1002af7
pietro2356 <pietro.rocchio@gmail.com>
2021-04-03 20:31:46 +0200

    Create le card e fixato il CSS.

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,6 @@
 const remoteDB = firebase.firestore();
 const type = [];
+const cards = [];
 
 
 /**
@@ -40,17 +41,28 @@ function getDbFromFirebase() {
             let data = doc.data();
             let piatto = new Piatto(doc.id, data.code, data.nome, data.prezzo, data.calorie, data.note, data.imgPath, data.section, data.tipo);
             //console.log(doc.id, " => ", doc.data());
-            //console.log(piatto.toString());
+            console.log(piatto.toString());
 
 
             // Creazione dei divisori per i piatti
             if (!type.includes(piatto.tipo)) {
                 type.push(piatto.tipo);
 
+                // Container generale
                 let cont = document.getElementById("container");
-                let containerType = document.createElement('div');
-                containerType.className = "container";
+
+                // Tipo di containter
+                let containerType = document.createElement('section');
+                containerType.className = "container clearfix";
                 containerType.id = piatto.tipo;
+
+                // Titolo del Container
+                let h = document.createElement('h1');
+                let text = document.createTextNode(piatto.tipo);
+                h.appendChild(text);
+
+                // Append
+                containerType.appendChild(h);
                 cont.appendChild(containerType);
             }
 
@@ -59,12 +71,73 @@ function getDbFromFirebase() {
             //      1: Creare la card corrispondente all'oggetto Piatto
             //      2: Ordinarle in base ai parametri
 
-
-
+            // In base al tipo crea la card nel container corrispondente;
+            if (piatto.tipo === "appetizers") {
+                let containerAppetizer = document.getElementById("appetizers");
+                containerAppetizer.appendChild(createDyCard(piatto));
+            } else if (piatto.tipo === "salads") {
+                let containerSalad = document.getElementById("salads");
+                containerSalad.appendChild(createDyCard(piatto));
+            }
         });
     });
 }
 
+function createDyCard(piatto) {
+    //Card generale
+    let card = document.createElement("div");
+    card.className = "demo-card-square mdl-card mdl-shadow--2dp clearfix";
+    card.id = piatto.id;
+
+    // Immagine della card
+    let cardMedia = document.createElement("div");
+    cardMedia.className = "mdl-card__media";
+    let img = document.createElement("img");
+    img.src = piatto.imgPath;
+    cardMedia.appendChild(img);
+
+    // Testo della card
+    let txt = document.createElement("div");
+    txt.className = "mdl-card__supporting-text";
+    let data = document.createTextNode(piatto.nome + " " + piatto.prezzo);
+    txt.appendChild(data);
+
+
+    // Container dei bottoni
+    let btnContainer = document.createElement("div");
+    btnContainer.className = "mdl-card__actions";
+
+    // Bottoni della card
+    let btnAdd = document.createElement("button");
+    btnAdd.className = "mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect";
+    let txtBtnAdd = document.createTextNode("ADD");
+    btnAdd.appendChild(txtBtnAdd);
+    btnContainer.appendChild(btnAdd);
+
+    let btnDel = document.createElement("button");
+    btnDel.className = "mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect";
+    let txtBtnDel = document.createTextNode("DEL");
+    btnDel.appendChild(txtBtnDel);
+    btnContainer.appendChild(btnDel);
+
+    // Append necessari per comporre la CARD finale.
+    card.appendChild(cardMedia);
+    card.appendChild(txt);
+    card.appendChild(btnContainer);
+
+    return card;
+}
+
+/**
+ * @deprecated
+ */
 function createCard(piatto) {
-    console.error("Function not implemented yet!\n ID => " + piatto.id);
+    let card = "<div class='mdl-card__media' id='" + piatto.id + "'>"; // Header card.
+    card += "<img src='" + piatto.imgPath + "'></div><div class='mdl-card__supporting-text'>"; // Immagine card.
+    card += piatto.toString() + "</div>";
+
+    // Footer della card con bottoni.
+    card += "<div class='mdl-card__actions'><a class='mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect'>ADD 1</a><a class='mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect'>DEL 1</a></div></div>";
+
+    return card;
 }
